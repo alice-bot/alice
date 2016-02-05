a303e2946d7dabdbe5f3dd1ebfe27802892daed2
Adam Zaninovich <adam.zaninovich@gmail.com>
2016-02-05 13:50:48 -0600

    Evil update

--- lib/alice/handlers/random.ex
+++ lib/alice/handlers/random.ex
@@ -12,7 +12,7 @@ defmodule Alice.Handlers.Random do
   route ~r/\bdevops\b/i,                                   :devops
   route ~r/\bIT IS DECIDED\!?\b/,                          :it_is_decided
   route ~r/\bmind blown\b/i,                               :mind_blown
-  route ~r/\bthe game\b/i,                                 :the_game
+  route ~r/\bgames?\b/i,                                 :the_game
 
   route ~r/mic ?drop|drop( ?the)? ?mic/,                   :mic_drop
   route ~r/\bcach(e|ing) ?bug\??\b/,                       :cache_bug
@@ -38,7 +38,7 @@ defmodule Alice.Handlers.Random do
   def handle(conn, :devops),            do: "http://i.imgur.com/Ujxb5gn.png" |> reply(conn)
   def handle(conn, :it_is_decided),     do: "http://i.imgur.com/80PQSCo.gif" |> reply(conn)
   def handle(conn, :mind_blown),        do: "http://i.imgur.com/lr4KJPQ.gif" |> reply(conn)
-  def handle(conn, :the_game),          do: chance_reply(0.5, "http://i.imgur.com/Z8awIpt.png", conn)
+  def handle(conn, :the_game),          do: chance_reply(0.25, "http://i.imgur.com/Z8awIpt.png", "I lost the game", conn)
 
   # Random Choice
 
