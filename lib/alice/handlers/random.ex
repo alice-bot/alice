27732c381d3c18e27a73abb74365952aa3681e52
Matt Rogers <codemattr@gmail.com>
2016-02-08 14:58:49 -0600

    THIS. IS. SPARTA!

--- lib/alice/handlers/random.ex
+++ lib/alice/handlers/random.ex
@@ -10,6 +10,7 @@ defmodule Alice.Handlers.Random do
   route ~r/\bbusted\b/i,                                   :busted
   route ~r/\bdev[- ]?ops\b/i,                              :devops
   route ~r/\bIT IS DECIDED\!?\b/,                          :it_is_decided
+  route ~r/\bmadness\b/i,                                  :this_is_sparta
   route ~r/\bmind blown\b/i,                               :mind_blown
   route ~r/\bgames?\b/i,                                   :the_game
   route ~r/\bthanks,? alice\b/i,                           :thanks
@@ -38,6 +39,7 @@ defmodule Alice.Handlers.Random do
   def handle(conn, :devops),            do: "http://i.imgur.com/6sNQ3yt.jpg" |> reply(conn)
   def handle(conn, :it_is_decided),     do: "http://i.imgur.com/80PQSCo.gif" |> reply(conn)
   def handle(conn, :mind_blown),        do: "http://i.imgur.com/lr4KJPQ.gif" |> reply(conn)
+  def handle(conn, :this_is_sparta),    do: "http://i.imgur.com/ydJ3Vcr.jpg" |> reply(conn)
   def handle(conn, :the_game),          do: chance_reply(0.25, "http://i.imgur.com/Z8awIpt.png", "I lost the game", conn)
   def handle(conn, :thanks),            do: "no prob, bob" |> reply(conn)
   def handle(conn, :alice_love),        do: "aww, I love you too :wink:" |> reply(conn)
