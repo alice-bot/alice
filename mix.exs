efaf336a2f85a76863364deb4a7486d31889e6f9
Adam Zaninovich <adam.zaninovich@gmail.com>
2016-02-13 22:44:46 -0600

    version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Alice.Mixfile do
 
   def project do
     [app: :alice,
-     version: "0.0.2",
+     version: "0.0.3",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
