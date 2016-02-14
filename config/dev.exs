426671255f8b6b24a09c201cf424b85c3764928c
Adam Zaninovich <adam.zaninovich@gmail.com>
2016-02-14 03:23:47 -0600

    clean up Alice package
    convert to library supporting bot, not the actual bot itself

--- config/dev.exs
+++ config/dev.exs
@@ -1,7 +1,3 @@
 use Mix.Config
 
-config :alice, api_key: System.get_env("LAASY_SLACK_KEY")
-config :alice, :state_backend, :redis
-config :alice, :redis, "redis://localhost"
-
 config :logger, level: :info, truncate: 32_768
