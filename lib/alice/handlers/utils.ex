48818120e611a699e223d8c73ce454cecc5ef5ee
Adam Zaninovich <adam.zaninovich@gmail.com>
2016-03-05 01:22:11 -0600

    Restructures handlers and help system
    See [Alice-0.2.0-Changes](https://github.com/alice-bot/alice/wiki/Alice-0.2.0-Changes) for more information

--- lib/alice/handlers/utils.ex
+++ lib/alice/handlers/utils.ex
@@ -1,21 +1,27 @@
 defmodule Alice.Handlers.Utils do
   use Alice.Router
 
-  route ~r/\Aping\z/i, :ping
-  command ~r/\bping\z/i, :ping
+  route   ~r/\Aping\z/i,        :ping
+  command ~r/\bping\z/i,        :ping
+  command ~r/\bdebug state\z/i, :debug_state
+  command ~r/\bdebug slack\z/i, :debug_slack
+  command ~r/\bdebug conn\z/i,  :debug_conn
 
-  def handle(conn, :ping) do
+
+  @doc "`ping` - responds with signs of life"
+  def ping(conn) do
     ["PONG!", "Can I help you?", "Yes...I'm still here.", "I'm alive!"]
     |> random_reply(conn)
   end
 
-  command ~r/\bdebug state\z/i, :debug_state
-  command ~r/\bdebug slack\z/i, :debug_slack
-  command ~r/\bdebug conn\z/i, :debug_conn
+  @doc "`debug state` - the current state data for debugging"
+  def debug_state(conn), do: inspect(conn.state)|> format_code |> reply(conn)
 
-  def handle(conn, :debug_state), do: inspect(conn.state)|> format_code |> reply(conn)
-  def handle(conn, :debug_slack), do: inspect(conn.slack)|> format_code |> reply(conn)
-  def handle(conn, :debug_conn),  do: inspect(conn)|> format_code |> reply(conn)
+  @doc "`debug slack` - the current slack data for debugging"
+  def debug_slack(conn), do: inspect(conn.slack)|> format_code |> reply(conn)
+
+  @doc "`debug conn` - the current conn data for debugging"
+  def debug_conn(conn),  do: inspect(conn)|> format_code |> reply(conn)
 
   # Formats code for Slack
   defp format_code(code) do
