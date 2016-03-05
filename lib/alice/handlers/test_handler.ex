b150fc69cea59dad7403002e105c534940a0e2c8
Adam Zaninovich <adam.zaninovich@activenetwork.com>
2016-03-05 01:22:11 -0600

    Restructures handlers and help system
    See [Alice-0.2.0-Changes](https://github.com/alice-bot/alice/wiki/Alice-0.2.0-Changes) for more information

---
+++ lib/alice/handlers/test_handler.ex
@@ -0,0 +1,33 @@
+defmodule Alice.Handlers.TestHandler do
+  use Alice.Router
+
+  route   ~r/cmd1/,     :command1
+  command ~r/cmd1/,     :command1
+  command ~r/cmd2/i,    :command2
+  command ~r/cmd3/i,    :command3
+  command ~r/no docs/i, :no_docs
+
+  @doc "`cmd1`: does some stuff"
+  def command1(_conn), do: nil
+
+  @doc """
+  `cmd2`: does some other stuff
+  also this one is multiline
+  """
+  def command2(_conn), do: nil
+
+  @doc """
+  this one doesn't start with an
+  example so no @alice is added
+  """
+  def command3(_conn), do: nil
+
+  def no_docs(_conn), do: nil
+
+  @doc """
+  this function should not
+  appear in help because it's
+  not a route or command
+  """
+  def another_func, do: nil
+end
--- lib/alice/router/helpers.ex
+++ lib/alice/router/helpers.ex
@@ -3,14 +3,18 @@ defmodule Alice.Router.Helpers do
   Helpers to make replying easier in handlers
   """
 
+  alias Alice.Conn
+
   @doc """
   Reply to a message in a handler.
 
+  Takes a conn and a response string in any order.
   Sends `response` back to the channel that triggered the handler.
 
   Adds random tag to end of image urls to break Slack's img cache.
   """
-  def reply(response, conn=%{message: %{channel: channel}, slack: slack}) do
+  def reply(response, conn=%Conn{}), do: reply(conn, response)
+  def reply(conn=%Conn{message: %{channel: channel}, slack: slack}, response) do
     response
     |> uncache_images
     |> slack_api.send_message(channel, slack)
@@ -39,19 +43,21 @@ defmodule Alice.Router.Helpers do
   end
 
   @doc """
+  Takes a conn and a list of possible response in any order.
   Replies with a random element of the `list` provided.
   """
-  def random_reply(list, conn), do: list |> Enum.random |> reply(conn)
+  def random_reply(list, conn=%Conn{}), do: random_reply(conn, list)
+  def random_reply(conn=%Conn{}, list), do: list |> Enum.random |> reply(conn)
 
   @doc """
   Reply with random chance.
 
   Examples
 
-      > chance_reply(0.5, "this will be sent half the time, otherwise nothing will be sent")
-      > chance_reply(0.25, "this will be sent 25% of the time", "sent 75% of the time")
+      > chance_reply(conn, 0.5, "this will be sent half the time, otherwise nothing will be sent")
+      > chance_reply(conn, 0.25, "this will be sent 25% of the time", "sent 75% of the time")
   """
-  def chance_reply(chance, positive, negative \\ :noreply, conn=%Alice.Conn{}) do
+  def chance_reply(conn=%Conn{}, chance, positive, negative \\ :noreply) do
     case {:rand.uniform <= chance, negative} do
       {true,  _}        -> reply(positive, conn)
       {false, :noreply} -> conn
@@ -62,6 +68,8 @@ defmodule Alice.Router.Helpers do
   @doc """
   Delay a reply. Alice will show to be typing while the message is delayed.
 
+  The conn can be passed in first or last.
+
   Returns the task, not a conn. If you need to get the conn, you can
   use `Task.await(task)`, but this will block the handle process until the delay
   finishes. If you don't need the updated conn, simply return the conn that was
@@ -69,30 +77,30 @@ defmodule Alice.Router.Helpers do
 
   Examples
 
-      def handle(conn, :hello) do
+      def hello(conn) do
         "hello" |> delayed_reply(1000, conn)
         conn
       end
 
-      def handle(conn, :hello) do
-        task = delayed_reply("hello", 1000, conn)
+      def hello(conn) do
+        task = delayed_reply(conn, "hello", 1000)
         # other work...
         Task.await(task)
       end
   """
-  def delayed_reply(message, milliseconds, conn) do
-    task = Task.async fn ->
-      indicate_typing(conn)
+  def delayed_reply(msg, ms, conn=%Conn{}), do: delayed_reply(conn, msg, ms)
+  def delayed_reply(conn=%Conn{}, message, milliseconds) do
+    Task.async(fn ->
+      conn = indicate_typing(conn)
       :timer.sleep(milliseconds)
       reply(message, conn)
-    end
-    task
+    end)
   end
 
   @doc """
   Indicate typing.
   """
-  def indicate_typing(conn=%{message: %{channel: channel}, slack: slack}) do
+  def indicate_typing(conn=%Conn{message: %{channel: channel}, slack: slack}) do
     slack_api.indicate_typing(channel, slack)
     conn
   end
@@ -126,22 +134,22 @@ defmodule Alice.Router.Helpers do
       @doc """
       Get the state from an Alice.Conn struct, namespaced to this module
       """
-      def get_state(conn=%Alice.Conn{}, key, default \\ nil) do
-        Alice.Conn.get_state_for(conn, namespace(key), default)
+      def get_state(conn=%Conn{}, key, default \\ nil) do
+        Conn.get_state_for(conn, namespace(key), default)
       end
 
       @doc """
       Update the state of an Alice.Conn struct, namespaced to this module
       """
-      def put_state(conn=%Alice.Conn{}, key, value) do
-        Alice.Conn.put_state_for(conn, namespace(key), value)
+      def put_state(conn=%Conn{}, key, value) do
+        Conn.put_state_for(conn, namespace(key), value)
       end
 
       @doc """
       Deletes the entries in the state for a specific `key`.
       """
-      def delete_state(conn=%Alice.Conn{}, key) do
-        Alice.Conn.delete_state_for(conn, namespace(key))
+      def delete_state(conn=%Conn{}, key) do
+        Conn.delete_state_for(conn, namespace(key))
       end
 
       @doc """
@@ -164,14 +172,13 @@ defmodule Alice.Router.Helpers do
       """
       def match_commands(conn), do: match(commands, conn)
 
-      defp match(patterns, connection=%Alice.Conn{message: message}) do
+      defp match(patterns, connection=%Conn{message: message}) do
         patterns
         |> Enum.reduce(connection, fn({pattern, name}, conn) ->
           if Regex.match?(pattern, message.text) do
-            Logger.info("#{__MODULE__} is responding to #{Alice.Conn.user(conn)} with #{name}")
-            conn = conn
-                   |> Alice.Conn.add_captures(pattern)
-                   |> __MODULE__.handle(name)
+            Logger.info("#{__MODULE__} is responding to #{Conn.user(conn)} with #{name}")
+            conn = conn |> Conn.add_captures(pattern)
+            apply(__MODULE__, name, [conn])
           end
           conn
         end)
