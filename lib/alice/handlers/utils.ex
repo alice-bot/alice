b95e3c23090ac1069abc2fc9e100148a98b29ff8
Adam Zaninovich <adam.zaninovich@gmail.com>
2016-03-05 05:14:48 -0600

    Revert "adds info to utils"

--- lib/alice/handlers/utils.ex
+++ lib/alice/handlers/utils.ex
@@ -3,31 +3,16 @@ defmodule Alice.Handlers.Utils do
 
   route   ~r/\Aping\z/i,        :ping
   command ~r/\bping\z/i,        :ping
-  command ~r/\binfo\z/i,        :info
   command ~r/\bdebug state\z/i, :debug_state
   command ~r/\bdebug slack\z/i, :debug_slack
   command ~r/\bdebug conn\z/i,  :debug_conn
+
 
   @doc "`ping` - responds with signs of life"
   def ping(conn) do
     ["PONG!", "Can I help you?", "Yes...I'm still here.", "I'm alive!"]
     |> random_reply(conn)
   end
-
-  @doc "`info` - info about Alice"
-  def info(conn) do
-    %{total_heap_size: heap, stack_size: stack} = Process.group_leader
-                                                  |> Process.info
-                                                  |> Enum.into(%{})
-    conn
-    |> reply("Alice #{alice_version} - https://github.com/alice-bot")
-    |> reply("Total heap size: #{heap} - Stack size: #{stack}")
-  end
-
-  defp alice_version, do: alice_version(Application.loaded_applications)
-  defp alice_version([{:alice, _desc, version} | _apps]), do: version
-  defp alice_version([]), do: "Unknown Version"
-  defp alice_version([_app|apps]), do: alice_version(apps)
 
   @doc "`debug state` - the current state data for debugging"
   def debug_state(conn), do: inspect(conn.state)|> format_code |> reply(conn)
