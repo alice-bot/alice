7d78c0514b24a6ac04bc01029e37c02b7ce3e8c9
Adam Zaninovich <adam.zaninovich@gmail.com>
2016-03-15 17:16:59 -0500

    fixes broken Utils/info command

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Alice.Mixfile do
 
   def project do
     [app: :alice,
-     version: "0.3.1",
+     version: "0.3.2",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
