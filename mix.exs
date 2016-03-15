e8673b4a3b4e18fed0bb58476f75045a85cc0d74
Adam Zaninovich <adam.zaninovich@gmail.com>
2016-03-15 16:16:49 -0500

    fixes deps

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule Alice.Mixfile do
       {:dogma,            github: "lpil/dogma", only: :dev},
       {:earmark, ">= 0.0.0", only: :dev},
       {:ex_doc,  ">= 0.0.0", only: :dev},
-      {:poison,  "~> 2.0.0", override: true},
+      {:poison,  "~> 2.0.0", only: :dev, override: true},
       {:slack,   "~> 0.4.2"},
       {:poolboy, "~> 1.5.0"},
       {:redix,   "~> 0.3.0"}
