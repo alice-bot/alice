da40e8d1960e1001309fe3154f48e986cf68a9e7
Adam Zaninovich <adam.zaninovich@gmail.com>
2016-06-07 20:58:18 -0500

    update version to 0.3.5

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Alice.Mixfile do
 
   def project do
     [app: :alice,
-     version: "0.3.4",
+     version: "0.3.5",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
