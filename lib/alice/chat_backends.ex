c11c6f7dffc85b289dee6cdcbff18f7bcac23e6a
Adam Zaninovich <adam.zaninovich@gmail.com>
2016-06-13 15:52:16 -0700

    Alice.ChatBackends.start_link takes options and returns the pid

--- lib/alice/chat_backends.ex
+++ lib/alice/chat_backends.ex
@@ -1,8 +1,8 @@
 defmodule Alice.ChatBackends do
-  def start_link(adapter) do
-    Agent.start_link(fn -> adapter end, name: __MODULE__)
-    adapter.start_link
-    {:ok, adapter}
+  def start_link(adapter, opts \\ [start_adapter: true]) do
+    {:ok, pid} = Agent.start_link(fn -> adapter end, name: __MODULE__)
+    if opts[:start_adapter], do: adapter.start_link
+    {:ok, pid, adapter}
   end
 
   def selected_adapter do
