bc252023332fa7d131c2ca75bdcf96f6b7cc4185
Adam Zaninovich <adam.zaninovich@gmail.com>
2017-09-06 12:02:41 -0700

    update git repo in info command

--- lib/alice/handlers/utils.ex
+++ lib/alice/handlers/utils.ex
@@ -16,7 +16,7 @@ defmodule Alice.Handlers.Utils do
   def info(msg, %{name: botname}) do
     {total, process} = memory_info()
 
-    reply(msg, "Alice #{alice_version()} - https://github.com/adamzaninovich/a2")
+    reply(msg, "Alice #{alice_version()} - https://github.com/alice-bot/alice/tree/a2")
     reply(msg, "Running as: #{botname}")
     reply(msg, "Total memory: #{total}MB - Allocated to processes: #{process}MB")
   end
