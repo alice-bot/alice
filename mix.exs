7b0412f1580f08d592a5ff80655fee8e0e6e9738
Tyler Clemens <tyler.r.clemens@gmail.com>
2019-06-21 10:53:24 -0700

    Update minimum elixir version to 1.6 to support formatter

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Alice.Mixfile do
     [
       app: :alice,
       version: "0.3.7",
-      elixir: "~> 1.5",
+      elixir: "~> 1.6",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       description: "A Slack bot",
