449024cbf8be15981be192632daf35baac529645
Tyler Clemens <tyler.r.clemens@gmail.com>
2019-06-21 11:07:30 -0700

    Ignore route/2 and command/2 for formatter, also export this so other third party alice handlers can use the same formatting for their DSL

--- .formatter.exs
+++ .formatter.exs
@@ -1,3 +1,12 @@
+locals_without_parens = [
+  #Alice.Router
+  command: 2,
+  route: 2
+]
+
+
 [
+  locals_without_parens: locals_without_parens,
+  export: [locals_without_parens: locals_without_parens],
   inputs: ["*.{ex,exs}", "{config,lib,priv,test}/**/*.{ex,exs}"]
 ]
