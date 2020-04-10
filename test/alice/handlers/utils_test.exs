6bba99f8623dd3ba3f9b66973372ea711b966f93
Nathaniel Barnes <nathaniel.r.barnes@gmail.com>
2020-04-10 15:24:50 -0700

    Implemented new test_message method to actually test the routing

--- test/alice/handlers/utils_test.exs
+++ test/alice/handlers/utils_test.exs
@@ -1,16 +1,15 @@
 defmodule Alice.Handlers.UtilsTest do
   use ExUnit.Case
-  import Alice.Handlers.Case
-  import Alice.Handlers.Utils
+  use Alice.Handlers.Case, handlers: Alice.Handlers.Utils
 
   test "it should respond to a ping" do
-    ping(fake_conn())
+    send_test_message("ping")
 
     assert Enum.member?(["PONG!", "Can I help you?", "Yes...I'm still here.", "I'm alive!"], first_reply())
   end
 
   test "it should respond with info about the running bot" do
-    info(fake_conn())
+    send_test_message("<@alice> info")
 
     {:ok, version} = :application.get_key(:alice, :vsn)
     assert first_reply() == "Alice #{version} - https://github.com/alice-bot"
--- test/support/case.ex
+++ test/support/case.ex
@@ -23,11 +23,35 @@ defmodule Alice.Handlers.Case do
   end
 
   def fake_conn_with_text(text) do
-    %Alice.Conn{message: %{text: text, channel: :channel}, slack: :slack}
+    %Alice.Conn{message: %{text: text, channel: :channel, user: 0}, slack: %{users: ["fake_user"], me: %{id: "alice"}}}
   end
 
   def fake_conn_with_capture(message, capture_regex) do
     fake_conn_with_text(message)
     |> Alice.Conn.add_captures(capture_regex)
+  end
+
+  def send_test_message(message) do
+    conn = fake_conn_with_text(message)
+    case Alice.Conn.command?(conn) do
+      true  -> Alice.Router.match_commands(conn)
+      false -> Alice.Router.match_routes(conn)
+    end
+  end
+
+  defmacro __using__(opts \\ []) do
+    handlers = opts
+               |> Keyword.get(:handlers, [])
+               |> List.wrap()
+
+    quote do
+      import Alice.Handlers.Case
+
+      setup do
+        Alice.Router.start_link(unquote(handlers))
+
+        :ok
+      end
+    end
   end
 end
