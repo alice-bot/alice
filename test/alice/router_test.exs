5e8dfa494ba06db052b65ece6e36028dfd7f4f9e
Adam Zaninovich <adam.zaninovich@gmail.com>
2020-04-11 16:36:52 -0700

    update router tests to use the new testing tools

--- test/alice/router_test.exs
+++ test/alice/router_test.exs
@@ -1,25 +1,20 @@
-defmodule TestHandler do
+defmodule Alice.RouterTest.TestHandler do
   use Alice.Router
 
   route ~r/pattern/, :my_route
   command ~r/pattern/, :my_route
 
   def my_route(conn) do
-    send(self(), {:received, routes()})
-    conn
+    reply(conn, "received my_route")
   end
 end
 
 defmodule Alice.RouterTest do
-  use ExUnit.Case, async: true
+  use Alice.HandlersCase, handlers: Alice.RouterTest.TestHandler
+
+  alias Alice.RouterTest.TestHandler
   alias Alice.Router
   alias Alice.Conn
-
-  setup do
-    Router.start_link([TestHandler])
-    :ok
-    Logger.configure(level: :warn)
-  end
 
   test "it remembers routes" do
     assert TestHandler.routes() == [{~r/pattern/, :my_route}]
@@ -40,29 +35,27 @@ defmodule Alice.RouterTest do
   end
 
   test "match_routes calls match_routes on each handler" do
-    {%{text: "pattern", user: "foo"}, %{users: %{"foo" => %{name: "foo"}}}, :state}
-    |> Conn.make()
-    |> Router.match_routes()
+    conn = fake_conn("pattern")
+    Router.match_routes(conn)
 
-    assert_received {:received, [{~r/pattern/, :my_route}]}
+    assert first_reply() == "received my_route"
   end
 
   test "match_commands calls match_commands on each handler" do
-    {%{text: "pattern", user: "foo"}, %{users: %{"foo" => %{name: "foo"}}}, :state}
-    |> Conn.make()
-    |> Router.match_commands()
+    conn = fake_conn("pattern")
+    Router.match_commands(conn)
 
-    assert_received {:received, [{~r/pattern/, :my_route}]}
+    assert first_reply() == "received my_route"
   end
 
   test "it can put state" do
-    conn = Alice.Conn.make(:msg, :slk)
+    conn = Conn.make(:message, :slack)
     conn = TestHandler.put_state(conn, :key, :val)
     assert conn.state == %{{TestHandler, :key} => :val}
   end
 
   test "it can get state" do
-    conn = Alice.Conn.make(:msg, :slk, %{{TestHandler, :key} => :val})
+    conn = Conn.make(:message, :slack, %{{TestHandler, :key} => :val})
     assert :val == TestHandler.get_state(conn, :key)
   end
 end
