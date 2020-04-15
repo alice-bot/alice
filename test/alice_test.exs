7f005ac4ed8c786349eaf3d7eb594947f387cbd8
Nathaniel Barnes <nathaniel.r.barnes@gmail.com>
2020-04-15 12:13:30 -0700

    Fixed the AliceTest

--- test/alice_test.exs
+++ test/alice_test.exs
@@ -1,11 +1,13 @@
-defmodule AliceTest.TestHandler do
-  use Alice.Router
-end
-
 defmodule AliceTest do
   use ExUnit.Case
 
-  alias Alice.Handlers.TestHandler
+  setup do
+    Application.delete_env(:alice, :handlers)
+  end
+
+  test "contains a default set of handlers" do
+    assert [Alice.Earmuffs, Alice.Handlers.Help, Alice.Handlers.Utils] == Alice.handlers(%{})
+  end
 
   test "properly adds handlers to the list when they're provided" do
     assert [Alice.Earmuffs, Alice.Handlers.Help, Alice.Handlers.Utils, TestHandler] ==
